2316b5f457265876183baa4256f25f3a1db70f0f
sapnavastre <sapnavastre@gmail.com>
2021-03-12 21:52:38 +0100

    imported correct file

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './collection-item.component.jsx';
+import './collection-item.styles.scss';
 
 const collectionItem = ({id, name, price, imageUrl}) => (
     <div className='collection-item'>
