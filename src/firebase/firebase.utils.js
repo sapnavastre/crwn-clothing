f440037286033991feaf3596668e296692020eef
sapnavastre <sapnavastre@gmail.com>
2021-04-07 15:17:07 +0200

    Redux

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,16 +2,25 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65',
-};
+// const config = {
+//   apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
+//   authDomain: 'crwn-db.firebaseapp.com',
+//   databaseURL: 'https://crwn-db.firebaseio.com',
+//   projectId: 'crwn-db',
+//   storageBucket: 'crwn-db.appspot.com',
+//   messagingSenderId: '850995411664',
+//   appId: '1:850995411664:web:7ddc01d597846f65',
+// };
 
+const config = {
+  apiKey: "AIzaSyBzp66AuUPZTiC4XlACQsXg4u8hPMsQS1E",
+  authDomain: "crwn-db-29a15.firebaseapp.com",
+  projectId: "crwn-db-29a15",
+  storageBucket: "crwn-db-29a15.appspot.com",
+  messagingSenderId: "742613401118",
+  appId: "1:742613401118:web:06549d2afe5f422d8e780f",
+  measurementId: "G-X06MJ82G2P"
+};
 firebase.initializeApp(config);
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
